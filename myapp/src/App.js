56e5774b461510042b3ffef58e367ba4f73dfa5d
PrakrutiChandak <prakrutichandak@gmail.com>
2021-03-13 00:16:42 +0530

    creating basic react app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './NavBar';
+
+function App() {
+    return ( <
+        div className = "App" >
+          <h1> My first react app</h1>
+          <p> Welcome :D</p>
+          <NavBar />
+        </div>
+    );
+}
+
+export default App;
---
+++ myapp/src/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return(
+            <div className="navbar">
+                <div>Name: Prakruti</div>
+                <div>Age: 23</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
